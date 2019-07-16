b98e8eb4bf63a57f5b3c92298abac91a9d6e23e0
Shuntaro Aoki <ShuntaroAoki@users.noreply.github.com>
2019-07-16 19:07:06 +0900

    Update email

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
           version='1.0.1',
           description='API for brain decoding datasets',
           author='Shuntaro Aoki',
-          author_email='kamitanilab.contact@gmail.com',
+          author_email='brainliner-admin@atr.jp',
           url='https://github.com/KamitaniLab/brain-decoding-datasets',
           license='MIT',
           packages=['bdds'],
